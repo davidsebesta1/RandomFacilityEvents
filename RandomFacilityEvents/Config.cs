7e1c3d4a7cfd701c2aa7dbbb840fe4b457a0f86b
davidsebesta1 <david.sebesta@post.cz>
2023-03-12 17:25:27 +0100

    Add project files.

---
+++ RandomFacilityEvents/Config.cs
@@ -0,0 +1,31 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomFacilityEvents.Plugin
+{
+    internal class Config
+    {
+        //Plugin enable
+        [Description("Is plugin enabled")]
+        public bool IsEnabled { get; set; } = true;
+
+        //Room related randomization
+        [Description("Randomized blackouts around facility rooms")]
+        public bool RandomRoomBlackouts { get; set; } = true;
+        [Description("Randomized zone-wide blackouts")]
+        public bool RandomZoneBlackouts { get; set; } = true;
+
+        //Item related randomization
+        [Description("Spawn random items around facility")]
+        public bool RandomItemSpawn { get; set; } = true;
+        [Description("Amount of items to spawn around facility")]
+        public int RandomItemSpawnAmount { get; set; } = 5;
+        [Description("Randomized add to items")]
+        public int MaximumAdditionalRandomAmount { get; set; } = 0;
+    }
+}
---
+++ RandomFacilityEvents/Plugin.cs
@@ -0,0 +1,71 @@
+﻿using PluginAPI.Core.Attributes;
+using PluginAPI.Core;
+using Log = PluginAPI.Core.Log;
+using PluginAPI.Enums;
+using System;
+using PluginAPI.Events;
+using System.Collections.Generic;
+using PluginAPI.Core.Zones;
+using MEC;
+using UnityEngine;
+using Random = System.Random;
+using InventorySystem;
+using InventorySystem.Items.Pickups;
+
+namespace RandomFacilityEvents.Plugin
+{
+    internal class Plugin
+    {
+        [PluginConfig]
+        public Config config;
+
+        [PluginEntryPoint("RandomFacilityEvents", "0.0.1", "Spawns random events around facility", "davidsebesta")]
+        public void Startup()
+        {
+            Log.Info("RandomFacilityEvents plugin is " + (config.IsEnabled ? "enabled" : "disabled"));
+            if(config.IsEnabled)
+            {
+                EventManager.RegisterEvents((object)this);
+            }
+        }
+
+        [PluginEvent(ServerEventType.RoundStart)]
+        public void RoundStart()
+        {
+            if (config.RandomItemSpawn)
+            {
+                Random random = new Random();
+
+                int TotalItemsToSpawn = config.RandomItemSpawnAmount + random.Next(config.MaximumAdditionalRandomAmount);
+
+                Timing.CallDelayed(1f, (Action)(() =>
+                {
+                    List<FacilityRoom> rooms = Facility.Rooms;
+                    for (int i = 0; i < TotalItemsToSpawn; i++)
+                    {
+                        FacilityRoom room = rooms[random.Next(rooms.Count)];
+
+
+                        SpawnItemAtRoom(ItemType.KeycardGuard, room);
+                    }
+
+                }));
+
+            }
+        }
+
+        private void SpawnItemAtRoom(ItemType itemType, FacilityRoom room)
+        {
+            var item = Server.Instance.ReferenceHub.inventory.ServerAddItem(itemType);
+            ItemPickupBase itemPickup = item.ServerDropItem();
+
+            if (itemPickup != null)
+            {
+                itemPickup.transform.position = room.Position;
+                itemPickup.transform.rotation = Quaternion.identity;
+            }
+
+            Log.Info("Spawned " + itemType + " at " + room.Position + " which is "  + room.Identifier);
+        }
+    }
+}
